61be430a2fa8e02ff979e7ff1fb2274816946b64
jonathanrodriguezcabrera1811-a11y <jonathanrodriguezcabrera1811@gmail.com>
2026-04-15 19:42:43 -0600

    Librerias y estructura basica

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot
+import seaborn as sbs
+from scipy.stats import norm 
+
+st.title("Proyecto distribuciones y prueba de hipotesis")
+st.write("Visualizaciones, prueba Z e integracion con la IA de Gemini")
